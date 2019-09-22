c84b589af2fb93cec5ba227ee8cf81e322793710
EC2 Default User <ec2-user@ip-10-0-0-38.eu-west-1.compute.internal>
2019-09-22 13:30:55 +0000

    first aws version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # tutti-crawler
+
+## fork
+This fork includes aws lambda functions and api definitions which can be used with a telegram bot. Because it is so far only intended to be for a small audience efficientcy will only be implemented later on
+
+### api
+`curl -o tutti.json -H "X-Tutti-Hash: "a275c2cc-8f54-4444-87a1-8b743dc868d4"" https://api.tutti.ch/v10/list.json?limit=30&o=1&region=4&sp=1&with_all_regions=true`
+
+## Setup
+1. `lambda_function.py` is a aws lambda function for it to work a Telegram bot is required and messages need to be forwarded to this function by aws API Gateway. For the setup you can consult `https://dev.to/nqcm/-building-a-telegram-bot-with-aws-api-gateway-and-aws-lambda-27fg` a Telegram webhook should be placed to point to your aws gateway. The Telegramm bot key must be submitted to the function by a enviromental variable named `TELEGRAM_TOKEN`
+2. Creatte a dynamodb table named "Tutti" and add a first item to the table by running `dynamodb_setup.py`
+3. create a Cloudwatch event to call the function e.g. every 15 minutes. 
---
+++ dynamo_setup.py
@@ -0,0 +1,48 @@
+import boto3
+import json
+import time
+
+
+# boto3 is the AWS SDK library for Python.
+# The "resources" interface allow for a higher-level abstraction than the low-level client interface.
+# More details here: http://boto3.readthedocs.io/en/latest/guide/resources.html
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+table = dynamodb.Table('Tutti')
+
+
+# The BatchWriteItem API allows us to write multiple items to a table in one request.
+if False:
+    with table.batch_writer() as batch:
+        time = int(time.time()-10000)
+        print(time)
+        row = {"id": -1,
+                "epoch_time": time,
+        }
+        print(row) 
+        batch.put_item(Item=row)
+
+if False:
+    with table.batch_writer() as batch:
+        row = {"id": 0,
+                "chat_id": {229891194},
+        }
+        print(row) 
+        batch.put_item(Item=row)
+        
+        row = {"id": 229891194,
+                "keywords": {'car'},
+        }
+        print(row) 
+        batch.put_item(Item=row)
+
+table.update_item(
+                Key={'id': 229891194},
+                UpdateExpression='add keywords :val',
+                ExpressionAttributeValues={':val': {'ikea'}}
+            )
+
+#check if we can retrieve item
+item = table.get_item(Key={'id': -1})
+epoch_time = item.get('Item').get('epoch_time')
+print(epoch_time)
+
